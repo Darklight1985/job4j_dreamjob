214d0111195565e1454ba5eee650ffdd588798a2
Dmitrij Kolesnev <darklight1985@mail.ru>
2022-02-17 19:22:37 +0300

    1. DbStore. [#281226 #265659] исправил

--- src/main/java/ru/job4j/dream/store/DbStore.java
+++ src/main/java/ru/job4j/dream/store/DbStore.java
@@ -12,11 +12,17 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Properties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DbStore implements Store {
-    private static final DbStore INSTANCE = new DbStore();
 
+    private static final Logger LOG = LoggerFactory.getLogger(DbStore.class.getName());
     private final BasicDataSource pool = new BasicDataSource();
+
+    private static final class Lazy {
+        private static final Store INST = new DbStore();
+    }
 
     private DbStore() {
         Properties cfg = new Properties();
@@ -45,7 +51,7 @@ public class DbStore implements Store {
     }
 
     public static Store instOf() {
-        return INSTANCE;
+        return Lazy.INST;
     }
 
     public Collection<Post> findAllPosts() {
@@ -61,7 +67,7 @@ public class DbStore implements Store {
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.debug("Exception: ", e.toString());
         }
         return posts;
     }
@@ -78,7 +84,7 @@ public class DbStore implements Store {
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.debug("Exception: ", e.toString());
         }
         return candidates;
     }
@@ -88,6 +94,14 @@ public class DbStore implements Store {
             create(post);
         } else {
             update(post);
+        }
+    }
+
+    public void save(Candidate candidate) {
+        if (candidate.getId() == 0) {
+            create(candidate);
+        } else {
+            update(candidate);
         }
     }
 
@@ -108,9 +122,29 @@ public class DbStore implements Store {
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.debug("Exception: ", e.toString());
         }
         return post;
+    }
+
+    private Candidate create(Candidate candidate) {
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps =
+                     cn.prepareStatement(
+                             "INSERT INTO candidate(name) VALUES (?)",
+                             PreparedStatement.RETURN_GENERATED_KEYS)
+        ) {
+            ps.setString(1, candidate.getName());
+            ps.execute();
+            try (ResultSet id = ps.getGeneratedKeys()) {
+                if (id.next()) {
+                    candidate.setId(id.getInt(1));
+                }
+            }
+        } catch (Exception e) {
+            LOG.debug("Exception: ", e.toString());
+        }
+        return candidate;
     }
 
     private void update(Post post) {
@@ -126,7 +160,22 @@ public class DbStore implements Store {
             ps.setInt(4, post.getId());
             ps.execute();
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.debug("Exception: ", e.toString());
+        }
+    }
+
+    private void update(Candidate candidate) {
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps =
+                     cn.prepareStatement(
+                             "UPDATE candidate SET name = ?"
+                                     + " WHERE id = ?")
+        ) {
+            ps.setString(1, candidate.getName());
+            ps.setInt(2, candidate.getId());
+            ps.execute();
+        } catch (Exception e) {
+            LOG.debug("Exception: ", e.toString());
         }
     }
 
@@ -141,8 +190,44 @@ public class DbStore implements Store {
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.debug("Exception: ", e.toString());
         }
         return null;
+    }
+
+    public Candidate findByIdCan(int id) {
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps = cn.prepareStatement("SELECT * FROM candidate WHERE id = ?")
+        ) {
+            ps.setInt(1, id);
+            try (ResultSet it = ps.executeQuery()) {
+                if (it.next()) {
+                    return new Candidate(it.getInt("id"), it.getString("name"));
+                }
+            }
+        } catch (Exception e) {
+            LOG.debug("Exception: ", e.toString());
+        }
+        return null;
+    }
+
+    public void deletePost(int id) {
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps = cn.prepareStatement("DELETE * FROM post WHERE id = ?")
+        ) {
+            ps.setInt(1, id);
+        } catch (Exception e) {
+            LOG.debug("Exception: ", e.toString());
+        }
+    }
+
+    public void deleteCan(int id) {
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps = cn.prepareStatement("DELETE * FROM candidate WHERE id = ?")
+        ) {
+            ps.setInt(1, id);
+        } catch (Exception e) {
+            LOG.debug("Exception: ", e.toString());
+        }
     }
 }
