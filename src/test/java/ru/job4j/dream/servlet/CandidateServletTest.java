c51201e8cc0cff086766282786aed0d993cc6a49
Dmitrij Kolesnev <darklight1985@mail.ru>
2022-02-22 18:29:19 +0300

    1. Написать тест на PostServlet. [#2518 #267539] исправил 4

--- src/test/java/ru/job4j/dream/servlet/CandidateServletTest.java
+++ src/test/java/ru/job4j/dream/servlet/CandidateServletTest.java
@@ -1,13 +1,9 @@
 package ru.job4j.dream.servlet;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
 import ru.job4j.dream.model.Candidate;
-import ru.job4j.dream.model.Post;
-import ru.job4j.dream.model.User;
 import ru.job4j.dream.store.DbStore;
-import ru.job4j.dream.store.MemStore;
 import ru.job4j.dream.store.Store;
 
 import javax.servlet.RequestDispatcher;
@@ -15,45 +11,41 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import java.io.IOException;
-import java.time.LocalDateTime;
 
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-public class PostServletTest {
+public class CandidateServletTest {
+
     @Test
-    public void whenCreatePost() throws IOException {
+    public void whenCreateCandidate() throws IOException {
         HttpServletRequest req = mock(HttpServletRequest.class);
         HttpServletResponse resp = mock(HttpServletResponse.class);
         Store store = DbStore.instOf();
         when(req.getParameter("id")).thenReturn("0");
-        when(req.getParameter("name")).thenReturn("name of new post");
-        when(req.getParameter("description")).thenReturn("something");
+        when(req.getParameter("name")).thenReturn("name of new candidate");
         try {
-            new PostServlet().doPost(req, resp);
+            new CandidateServlet().doPost(req, resp);
         } catch (ServletException e) {
             e.printStackTrace();
         }
-        Post post = store
-                .findAllPosts()
+        Candidate can = store
+                .findAllCandidates()
                 .stream()
-                .filter(s -> s.getName()
-                        .equals("name of new post"))
+                .filter(s -> s
+                        .getName()
+                        .equals("name of new candidate"))
                 .findFirst()
                 .get();
-        verify(resp).sendRedirect(String.format("%s/posts.do", req.getContextPath()));
-        assertThat(post.getDescription(), is("something"));
+        verify(resp).sendRedirect(String.format("%s/candidates.do", req.getContextPath()));
+        assertThat(can.getName(), is("name of new candidate"));
     }
 
     @Test
-    public void whenCreateGet() throws IOException {
+    public void whenGetCandidate() throws IOException {
         HttpServletRequest req = mock(HttpServletRequest.class);
         HttpServletResponse resp = mock(HttpServletResponse.class);
         HttpSession sc = mock(HttpSession.class);
@@ -61,10 +53,10 @@ public class PostServletTest {
         Mockito.when(req.getSession()).thenReturn(sc);
         Mockito.when(req.getRequestDispatcher(any())).thenReturn(requestDispatcher);
         try {
-            new PostServlet().doGet(req, resp);
+            new CandidateServlet().doGet(req, resp);
         } catch (ServletException e) {
             e.printStackTrace();
         }
-        verify(req).getRequestDispatcher("posts.jsp");
+        verify(req).getRequestDispatcher("candidates.jsp");
     }
 }
